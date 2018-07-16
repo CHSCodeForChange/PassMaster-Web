e05be549b8d3cec3719e1ccd5f12faa7f4102485
computerquest <jaredstigter@gmail.com>
2018-07-15 20:38:23 -0700

    set the pass type made the teachers so you can choose from

--- Student/forms.py
+++ Student/forms.py
@@ -8,14 +8,8 @@ from Teacher.models import Teacher
 
 
 class RequestForm(forms.Form):
-    location = forms.CharField(label='', required=True, max_length=240, widget=forms.TextInput(
-        attrs={'type': 'text',
-               'class': 'form-control',
-               'placeholder': 'To Teacher (Last Name)'}))
-    sending = forms.CharField(label='', required=True, max_length=240, widget=forms.TextInput(
-        attrs={'type': 'text',
-               'class': 'form-control',
-               'placeholder': 'From Teacher (Last Name)'}))
+    destinationTeacher = forms.ModelChoiceField(queryset=Teacher.objects.all(), empty_label=None)
+    originTeacher = forms.ModelChoiceField(queryset=Teacher.objects.all(), empty_label=None)
     start = forms.DateTimeField(label='Start time', input_formats=['%Y-%m-%dT%H:%M'],
                                 widget=forms.DateTimeInput(
                                     attrs={'type': 'datetime-local',
@@ -36,27 +30,12 @@ class RequestForm(forms.Form):
         self.user = kwargs.pop('user')
         super(RequestForm, self).__init__(*args, **kwargs)
 
-    def clean_sending(self):
-        b = self.cleaned_data['sending']
-
-        if Teacher.objects.filter(idName=b).exists():
-            return b
-        else:
-            raise ValidationError('orgin teacher is invalid')
-
-    def clean_location(self):
-        b = self.cleaned_data['location']
-
-        if Teacher.objects.filter(idName=b).exists():
-            return b
-        else:
-            raise ValidationError('recieving teacher is invalid')
     def save(self, commit=True):
         print(self.user)
         student = Student.objects.get(profile=self.user.profile)
 
         newPass = Pass(approved=False, startTimeRequested=self.cleaned_data['start'],
                        endTimeRequested=self.cleaned_data['end'], description=self.cleaned_data['reason'],
-                       student=student, destinationTeacher=Teacher.objects.get(idName=self.cleaned_data['location']),
-                       originTeacher=Teacher.objects.get(idName=self.cleaned_data['sending']))
+                       student=student, destinationTeacher=self.cleaned_data['destinationTeacher'],
+                       originTeacher=self.cleaned_data['originTeacher'], type='1')
         return newPass
