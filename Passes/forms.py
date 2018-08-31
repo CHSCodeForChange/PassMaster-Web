c766b1be4a67c52cf1ca12358474451e43632c2c
Evan Kenyon <evan.kenyon123@gmail.com>
2018-08-31 14:45:12 -0400

    Ignore this commit my computer is being janky

--- Passes/forms.py
+++ Passes/forms.py
@@ -1,52 +1,142 @@
+from dal import autocomplete
+
 from django import forms
 from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError
-from django.contrib.auth import models
-from datetime import datetime, timezone
 
-from Passes.models import Pass
+from .models import *
 
-class NewPassForm(forms.Form):
-    description = forms.CharField(label='Description', max_length=960, widget=forms.TextInput(
+
+class TeacherPassForm(forms.ModelForm):
+	originTeacher = forms.ModelChoiceField(
+		queryset=Teacher.objects.all(),
+		widget=autocomplete.ModelSelect2(url='/teacher/autocomplete')
+	)
+
+	class Meta:
+		model = TeacherPass
+		fields = ('__all__')
+
+
+class RequestPassForm(forms.Form):
+	pass_type = forms.CharField(max_length=1, widget=forms.HiddenInput(), initial="1")
+
+	destinationTeacher = forms.ModelChoiceField(queryset=Teacher.objects.all(), empty_label=None,
+	                                            label="Destination teacher", required=False, )
+
+	location = forms.CharField(max_length=12, required=False, widget=forms.TextInput(
         attrs={'type': 'text',
-               'class': 'form-control form'}))
+               'class': 'form-control',
+               'placeholder': 'Location',
+               'style': 'display: none;'}))
 
-    student = models.User()
-    destinationTeacher = forms.ModelChoiceField(queryset=User.objects.all(), empty_label=None, widget=forms.Select(
+	originTeacher = forms.ModelChoiceField(
+		queryset=Teacher.objects.all(),
+		widget=autocomplete.ModelSelect2(url='/teacher/autocomplete')
+	)
+
+	date = forms.DateField(label='Date', required=True, input_formats=['%Y-%m-%d'],
+	                       initial=datetime.now, widget=forms.DateInput(
+			attrs={'type': 'date',
+			       'class': 'form-control'}))
+
+	start = forms.TimeField(label='Start time', required=True, input_formats=['%H:%M'],
+	                        widget=forms.TimeInput(
+		                        attrs={'type': 'time',
+		                               'class': 'form-control'}))
+	end = forms.TimeField(label='End time', required=True, input_formats=['%H:%M'],
+	                      widget=forms.TimeInput(
+		                      attrs={'type': 'time',
+		                             'class': 'form-control'}))
+
+	reason = forms.CharField(label='', required=True, max_length=240, widget=forms.TextInput(
         attrs={'type': 'text',
-               'class': 'form-control form'}))
-    originTeacher = forms.ModelChoiceField(queryset=User.objects.all(), empty_label=None, widget=forms.Select(
-        attrs={'type': 'text',
-               'class': 'form-control form'}))
+               'class': 'form-control',
+               #    'rows': '3'
+               'placeholder': 'Reason for pass'}))
 
-    timeOfSession = forms.DateTimeField(widget=forms.Select(
-        attrs={'type': 'text',
-               'class': 'form-control form'}))
+	user = User()
 
-    timeOfRequest = datetime.now()
+	def __init__(self, *args, **kwargs):
+		self.user = kwargs.pop('user')
+		super(RequestPassForm, self).__init__(*args, **kwargs)
 
-    session = forms.ChoiceField(label="",
-                                initial='',
-                                widget=forms.Select(),
-                                required=True)
+	def save(self, commit=True):
+		student = Student.objects.get(profile=self.user.profile)
+		if self.cleaned_data['pass_type'] == '1':
+			new_pass = TeacherPass(approved=False, date=self.cleaned_data['date'],
+			                       startTimeRequested=self.cleaned_data['start'],
+			                       endTimeRequested=self.cleaned_data['end'], description=self.cleaned_data['reason'],
+			                       student=student, destinationTeacher=self.cleaned_data['destinationTeacher'],
+			                       originTeacher=self.cleaned_data['originTeacher'])
+		else:
+			new_pass = LocationPass(approved=False, date=self.cleaned_data['date'],
+			                        startTimeRequested=self.cleaned_data['start'],
+			                        endTimeRequested=self.cleaned_data['end'], description=self.cleaned_data['reason'],
+			                        student=student, location=self.cleaned_data['location'],
+			                        originTeacher=self.cleaned_data['originTeacher'])
+		new_pass.save()
 
 
-    def __init__(self, *args, **kwargs):
-        self.owner = kwargs.pop('owner')
-        super(NewPassForm, self).__init__(*args, **kwargs)
-    def clean_description(self):
-        description = self.cleaned_data['description']
-        return description
-    def clean_time(self):
-        time = self.cleaned_data['timeOfSession']
-        return time
+# Teacher
 
-    def save(self, commit=True):
-        group = Group(
-            description=self.cleaned_data['description'],
-            destinationTeacher=destinationTeacher,
-            originTeacher=originTeacher,
-            timeOfSession=timeOfSession,
-            timeOfRequest=timeOfRequest,
-            )
-        return pass
+
+class CreatePassForm(forms.Form):
+	pass_type = forms.CharField(max_length=1, widget=forms.HiddenInput(), initial="1")
+
+	destinationTeacher = forms.ModelChoiceField(queryset=Teacher.objects.all(), empty_label=None,
+	                                            label="Destination teacher", required=False, )
+
+	location = forms.CharField(max_length=12, required=False, widget=forms.TextInput(
+		attrs={'type': 'text',
+		       'class': 'form-control',
+		       'placeholder': 'Location',
+		       'style': 'display: none;'}))
+
+	students = forms.ModelMultipleChoiceField(queryset=Student.objects.all(), label="Student(s)", required=True)
+
+	date = forms.DateField(label='Date', required=True, input_formats=['%Y-%m-%d'],
+	                       initial=datetime.now, widget=forms.DateInput(
+			attrs={'type': 'date',
+			       'class': 'form-control'}))
+
+	start = forms.TimeField(label='Start time', required=True, input_formats=['%H:%M'],
+	                        widget=forms.TimeInput(
+		                        attrs={'type': 'time',
+		                               'class': 'form-control'}))
+	end = forms.TimeField(label='End time', required=True, input_formats=['%H:%M'],
+	                      widget=forms.TimeInput(
+		                      attrs={'type': 'time',
+		                             'class': 'form-control'}))
+
+	reason = forms.CharField(label='', required=True, max_length=240, widget=forms.TextInput(
+		attrs={'type': 'text',
+		       'class': 'form-control',
+		       'placeholder': 'Reason for pass'}))
+
+	user = User()
+
+	def __init__(self, *args, **kwargs):
+		self.user = kwargs.pop('user')
+		super(CreatePassForm, self).__init__(*args, **kwargs)
+
+	def save(self, commit=True):
+		originTeacher = Teacher.objects.get(profile=self.user.profile)
+
+		if self.cleaned_data['pass_type'] == '1':
+			for student in self.cleaned_data['students']:
+				new_pass = TeacherPass(approved=True, date=self.cleaned_data['date'],
+				                       startTimeRequested=self.cleaned_data['start'],
+				                       endTimeRequested=self.cleaned_data['end'],
+				                       description=self.cleaned_data['reason'],
+				                       student=student, destinationTeacher=self.cleaned_data['destinationTeacher'],
+				                       originTeacher=originTeacher)
+				new_pass.save()
+		else:
+			for student in self.cleaned_data['students']:
+				new_pass = LocationPass(approved=True, date=self.cleaned_data['date'],
+				                        startTimeRequested=self.cleaned_data['start'],
+				                        endTimeRequested=self.cleaned_data['end'],
+				                        description=self.cleaned_data['reason'],
+				                        student=student, location=self.cleaned_data['location'],
+				                        originTeacher=originTeacher)
+				new_pass.save()
--- Passes/urls.py
+++ Passes/urls.py
@@ -1,11 +1,12 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
+from django.views.generic import RedirectView
 
-from . import views
+from passes.autocomp import TeacherAutocomplete
 from . import api
-
-from rest_framework import routers
+from . import views
 
 urlpatterns = [
+	# API
     url(r'^api/passes/$', api.PassList.as_view()),
 
     url(r'^api/passes/(?P<pk>[0-9]+)/$', api.PassGet.as_view()),
@@ -18,5 +19,23 @@ urlpatterns = [
     url(r'^api/passes/create/location/$', api.LocationPassCreate.as_view()),
     url(r'^api/passes/create/srt/$', api.SRTPassCreate.as_view()),
     url(r'^api/passes/create/teacher/$', api.TeacherPassCreate.as_view()),
+
+	# Student
+	url(r'^student/request/', views.requestPass),
+	url(r'^student/requestTeacherPass/', views.requestTeacherPass),
+	url(r'^student/$', views.student_home, name='student_home'),
+	url(r'^student/', RedirectView.as_view(pattern_name='student_home', permanent=False)),
+
+	# Teacher
+	url(r'^teacher/approve/(?P<pass_id>[0-9]+)/$', views.approve, name='approve'),
+	url(r'^teacher/checkout/(?P<pass_id>[0-9]+)/$', views.checkout, name='checkout'),
+	url(r'^teacher/checkin/(?P<pass_id>[0-9]+)/$', views.checkin, name='checkin'),
+	url(
+		r'^teacher/autocomplete/$',
+		TeacherAutocomplete.as_view(),
+		name='teacher-autocomplete',
+	),
+	url(r'^teacher/$', views.teacher_home, name='teacher_home'),
+	url(r'^teacher/', RedirectView.as_view(pattern_name='teacher_home', permanent=False)),
 
 ]
