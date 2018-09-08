8faf2b9d0ad67e17d832289dce17b213c8d4880c
Evan Kenyon <evan.kenyon123@gmail.com>
2018-09-08 00:26:30 -0400

    Deleted Profiles

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,9 +6,6 @@ from django.views.generic import ListView, DetailView
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
     url(r'^logout_lander/', views.logoutLander, name='logout_lander'),
-    url(r'^signup', views.signup, name='signup'),
-    url(r'^profile/$', views.profile, name='profile'),
-    url(r'^profile/(?P<user_id>[0-9]+)', views.other_profile, name="other_profile"),
+    url(r'^signup', views.signup, name='signup')
 ]
